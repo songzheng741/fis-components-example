c5c0f685c63cd7748418498d01d34b51d52f4a04
songzheng01 <songzheng01@baidu.com>
2015-04-22 23:46:14 +0800

    构建一个满足业务需求的树

---
+++ static/js/component/tree/Node.js
@@ -0,0 +1,14 @@
+var defaults = {
+    icon: '',
+    text: ''
+}
+
+function Node() {
+    this.id = 'node_0';
+    this.root = false;       //是否为根节点
+    this.isLeaf = false;     //是否为叶子节点
+    this.depth = 0;          //此节点所处深度
+    this.iconCls = '';       //图标样式
+    this.text = '';          //显示文字
+}
+
